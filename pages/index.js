81771de14635854f27cb5d98e92877307459db4c
ericksih <erick.saputra08@gmail.com>
2022-02-16 10:37:56 +0700

    wish me luck

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Try Next.js!</title>
+        <meta name='keywords' content='Here we go again!' />
+      </Head>
+
+      <h1> Hello, from Next! </h1>
+    </div>
+  );
+}
